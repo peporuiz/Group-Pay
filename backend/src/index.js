a294c5f5a1f521aa10be7d4c53096d6a47bf5fca
peporuiz <miel-tacticas-0h@icloud.com>
2023-07-05 20:04:21 -0400

    Version Final

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ import bodyParser from 'koa-body'
 import router from './routes/group/groups'
 
 const app = new Koa()
-const port = 3000
+const port = 8000
 
 app.use(bodyParser({multipart:true, urlencodede:true}))
 app.use(router.routes())
 
-app.listen(3000, () => {
+app.listen(8000, () => {
     console.log(`Server is running on port ${port}`)
 })
